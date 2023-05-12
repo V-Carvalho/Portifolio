6edbd3790d3f22d1e0fff5408997894be959708e
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-12 19:43:09 -0300

    ThemeContext/ThemeProvider finalizados

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import {ThemeProvider} from '../src/contexts/ThemeContext.jsx'
+import { ThemeProvider } from '../src/contexts/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
