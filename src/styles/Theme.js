95daf8d949c42861513adefc757038b46f74bbdc
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-13 19:41:05 -0300

    Tema Claro/Escuro finalizado

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -3,15 +3,18 @@ export const darkTheme = {
   titleColor: '#BBBBBB',
   borderColor: '#343434',
   primaryColor: '#1E1E1E',
-  secondaryColor: "#4A4A4A",
+  secondaryColor: "#343434",
   iconPrimaryColor: "#E8E8E8",
   iconSecondaryColor: "#6997D5",
 }
 
 export const lightTheme = {
-  background: 'red',
-  titles: '#2C2738',
-  text: '#2C2738',
-  accent: '#0880AE',
+  textColor: '#1E1E1E',
+  titleColor: '#4a4a4a',
+  borderColor: '#BBBBBB',
+  primaryColor: '#E8E8E8',
+  secondaryColor: "#BBBBBB",
+  iconPrimaryColor: "#1E1E1E",
+  iconSecondaryColor: "#6997D5",
 }
 
