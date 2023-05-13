9172baa045fb1ca135787bcc1b23337acef0a960
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-13 17:15:29 -0300

    Sidebar Finalizada ***Falta url do currículo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import styled, { useTheme } from "styled-components";
-
-import { links } from "../../utils/Links";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { VscFiles, VscSourceControl, VscGithubInverted } from "react-icons/vsc";
+import { BsLinkedin, BsEnvelopeAtFill, BsTelephoneFill, BsWhatsapp, BsSunFill, BsFillMoonFill, BsGearFill,  BsFileEarmarkArrowDownFill } from "react-icons/bs";
 
 const StyledSidebar = styled.aside`
   display: flex;
@@ -41,7 +41,83 @@ const SidebarIcon = styled.a`
 
 const Sidebar = () => {
   const theme = useTheme();
-  const { toggleTheme } = useContext(ThemeContext);
+  const { isDarkTheme, toggleTheme } = useContext(ThemeContext);
+
+  const links = [
+    [
+      {
+        index: 0,
+        title: "",
+        url: "",
+        icon: <VscFiles size={25} color={theme.iconPrimaryColor} />,
+      },
+      {
+        index: 1,
+        title: "Repositório",
+        url: "https://github.com/V-Carvalho/Portfolio",
+        icon: <VscSourceControl size={25} color={theme.iconPrimaryColor} />,
+      },
+      {
+        index: 2,
+        title: "Github",
+        url: "https://github.com/V-Carvalho",
+        icon: <VscGithubInverted size={25} color={theme.iconPrimaryColor} />,
+      },
+      {
+        index: 3,
+        title: "Linkedin",
+        url: "https://www.linkedin.com/in/vinicius-a-carvalho/",
+        icon: <BsLinkedin size={25} color={theme.iconPrimaryColor} />,
+      },
+      {
+        index: 4,
+        title: "E-mail",
+        url: "mailto:vinicius.a.carvalho@outlook.com",
+        icon: <BsEnvelopeAtFill size={25} color={theme.iconPrimaryColor} />,
+      },
+      {
+        index: 5,
+        title: "Telefone",
+        url: "tel:+5511980554113",
+        icon: <BsTelephoneFill size={25} color={theme.iconPrimaryColor} />,
+      },
+      {
+        index: 6,
+        title: "WhatsApp",
+        url: "https://api.whatsapp.com/send?phone=5511980554113&text=Fala%20Vinicius,%20Estou%20entrando%20em%20contato%20através%20do%20seu%20portfólio...",
+        icon: <BsWhatsapp size={25} color={theme.iconPrimaryColor} />,
+      },
+      {
+        index: 7,
+        title: "Download Currículo",
+        url: "",
+        icon: (
+          <BsFileEarmarkArrowDownFill
+            size={25}
+            color={theme.iconPrimaryColor}
+          />
+        ),
+      },
+    ],
+    [
+      {
+        index: 0,
+        title: "Tema",
+        url: "",
+        icon: isDarkTheme ? (
+          <BsSunFill size={25} color={theme.iconPrimaryColor} />
+        ) : (
+          <BsFillMoonFill size={25} color={theme.iconPrimaryColor} />
+        ),
+      },
+      {
+        index: 1,
+        title: "Configurações",
+        url: "",
+        icon: <BsGearFill size={25} color={theme.iconPrimaryColor} />,
+      },
+    ],
+  ];
 
   return (
     <StyledSidebar borderColor={theme.borderColor}>
