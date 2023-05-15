d479d6cd2fecf2e749d6d1683cb10c4f172591c6
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-15 11:40:42 -0300

    Removida a cor dos títulos / Tema Claro/Escuro finalizado

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,6 +1,5 @@
 export const darkTheme = {
   textColor: '#E8E8E8',
-  titleColor: '#BBBBBB',
   borderColor: '#343434',
   primaryColor: '#1E1E1E',
   secondaryColor: "#343434",
@@ -10,7 +9,6 @@ export const darkTheme = {
 
 export const lightTheme = {
   textColor: '#1E1E1E',
-  titleColor: '#4a4a4a',
   borderColor: '#BBBBBB',
   primaryColor: '#E8E8E8',
   secondaryColor: "#BBBBBB",
