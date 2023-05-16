d82b4d4cdf164795d951a407ac1684fa6a5ebee6
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-16 19:22:54 -0300

    Pag. formação finalizada

---
+++ src/pages/Formations/Formations.jsx
@@ -0,0 +1,53 @@
+import styled, { useTheme } from "styled-components";
+import { formations } from "../../utils/DataCV";
+
+import Header from "../../components/Header/Header";
+import TitlePage from "../../components/TitlePage/TitlePage";
+import Topic from "../../components/Topic/Topic";
+
+const Main = styled.main`
+  flex: 1;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContentMain = styled.section`
+  height: 100%;
+  display: flex;
+  overflow-y: auto;  
+  padding: 0px 50px;
+  overflow-x: hidden;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${(props) => props.backgroundColor};
+`;
+
+const Formations = () => {
+  const theme = useTheme();
+
+  return (
+    <Main>
+      <Header />
+      <ContentMain backgroundColor={theme.primaryColor}>
+        {formations.map((formation) => (
+          <>
+            <TitlePage
+              title={formation.title}
+              textColor={theme.textColor}
+              borderColor={theme.fixedColor}
+              iconColor={theme.iconPrimaryColor}
+            />
+            <Topic
+              topics={formation.topics}
+              textColor={theme.textColor}
+              backgroundColor={theme.fixedColor}
+            />
+          </>
+        ))}
+      </ContentMain>
+    </Main>
+  );
+};
+
+export default Formations;
