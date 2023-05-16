d91a03f9ade6f8175a18cf65b6550969d3093d91
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-16 19:22:00 -0300

    Add borda no título da página

---
+++ src/components/TitlePage/TitlePage.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { FaMedal } from "react-icons/fa";
+
+const ContainerTitle = styled.div`
+   flex: 0;
+   width: 100%;
+   display: flex;
+   padding: 15px 0px;
+   align-items: center;
+   flex-direction: row;
+   margin: 20px 0px 0px 0px;
+   border-bottom: thin solid ${(props) => props.borderColor};  
+`;
+
+const Title = styled.h1`
+  display: flex;
+  margin: 0px 15px;
+  font-weight: bold;
+  color: ${(props) => props.textColor};
+`;
+
+const TitleTopic = ({ title, textColor, borderColor, icon, iconColor }) => {
+  return (
+    <ContainerTitle borderColor={borderColor}>
+      <FaMedal size={25} color={iconColor} />
+      <Title textColor={textColor}>{title}</Title>
+    </ContainerTitle>
+  );
+}
+
+
+export default TitleTopic;
