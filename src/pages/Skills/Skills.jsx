2173ed234a9af37b7d4be3a0a35f23e6b276d5a0
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-16 19:17:32 -0300

    Pag. habilidades finalizada

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,37 +1,57 @@
 import styled, { useTheme } from "styled-components";
+import { skills } from "../../utils/DataCV";
+
 import Header from "../../components/Header/Header";
+import TitlePage from "../../components/TitlePage/TitlePage";
+import Topic from "../../components/Topic/Topic";
 
 const Main = styled.main`
-  flex: 3; // mudar p/ 1
+  flex: 1;
   height: 100vh;
   display: flex;
   flex-direction: column;
 `;
 
 const ContentMain = styled.section`
+  height: 100%;
   display: flex;
-  height: 100%;  
+  overflow-y: auto;
+  padding: 0px 50px;
+  overflow-x: hidden;
   align-items: center;
   flex-direction: column;
-  justify-content: center;
   background-color: ${(props) => props.backgroundColor};
-
-  color: whitesmoke;
 `;
 
 const Skills = () => {
   const theme = useTheme();
 
   return (
-    <Main >
+    <Main>
       <Header />
-      <ContentMain backgroundColor={theme.secondaryColor}>
-        <h1 color={theme.textColor}>Skills</h1>
+      <ContentMain backgroundColor={theme.primaryColor}>
+        {skills.map((skill) => (
+          <>
+            <TitlePage
+              title={skill.title}
+              textColor={theme.textColor}
+              borderColor={theme.fixedColor}
+              iconColor={theme.iconPrimaryColor}
+            />
+            <Topic
+              topics={skill.topics}
+              textColor={theme.textColor}
+              backgroundColor={theme.fixedColor}
+            />
+          </>
+        ))}
       </ContentMain>
     </Main>
   );
 };
 
 export default Skills;
+
+
 
 
