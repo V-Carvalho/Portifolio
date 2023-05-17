3e87801e3c4c62d900e4e2df86a6591cd2c41a5f
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-17 13:22:24 -0300

    Página projetos finalizada

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,64 @@
+import styled, { useTheme } from "styled-components";
+import { projects } from "../../utils/DataCV";
+
+import Header from "../../components/Header/Header";
+import TitlePage from "../../components/TitlePage/TitlePage";
+import Topic from "../../components/Topic/Topic";
+
+const Main = styled.main`
+  flex: 1;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContentMain = styled.section`
+  height: 100%;
+  display: flex;
+  overflow-y: auto;
+  padding: 0px 50px;
+  overflow-x: hidden;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${(props) => props.backgroundColor};
+`;
+
+const Projects = () => {
+  const theme = useTheme();
+
+  return (
+    <Main>
+      <Header />
+      <ContentMain backgroundColor={theme.primaryColor}>
+        {projects.map((project) => (
+          <>
+            <TitlePage
+              title={project.title}
+              textColor={theme.textColor}
+              borderColor={theme.fixedColor}
+              iconColor={theme.iconPrimaryColor}
+            />
+            <Topic
+              topics={project.topics}
+              textColor={theme.textColor}
+              iconColor={theme.iconPrimaryColor}
+              borderColor={theme.borderColor}
+              backgroundColorDate={theme.fixedColor}
+            />
+          </>
+        ))}
+      </ContentMain>
+    </Main>
+  );
+};
+
+export default Projects;
+
+
+
+
+
+
+
+
+
