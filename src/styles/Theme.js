a04661cf90f053477e6796d914cb9f506f6a8d97
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-17 10:32:39 -0300

    Alterada a cor secundaria do tema

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,6 +1,6 @@
 export const darkTheme = {
   textColor: '#E8E8E8',
-  borderColor: '#343434',
+  borderColor: '#4a4a4a',
   primaryColor: '#1E1E1E',
   secondaryColor: "#343434",
   iconPrimaryColor: "#E8E8E8",
@@ -12,7 +12,7 @@ export const lightTheme = {
   textColor: '#1E1E1E',
   borderColor: '#BBBBBB',
   primaryColor: '#E8E8E8',
-  secondaryColor: "#BBBBBB",
+  secondaryColor: "#d2d2d2",
   iconPrimaryColor: "#1E1E1E",
   iconSecondaryColor: "#6997D5",
   fixedColor: "#6997D5",
