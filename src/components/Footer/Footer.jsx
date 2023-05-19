1bf554b2c4ac7fdb530ab1ae814d7476369ed592
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-05-19 20:00:24 -0300

    Footer fixada

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,13 +2,14 @@ import styled from "styled-components";
 import { BiGitBranch } from "react-icons/bi";
 import { VscRemote, VscError, VscWarning, VscBell, VscFeedback, VscCheck } from "react-icons/vsc"; 
 
+
 const StyledFooter = styled.div`
   left: 0;
   bottom: 0;
   width: 100%;
   height: 20px;
   display: flex;
-  position: absolute;
+  position: fixed;
   flex-direction: row;
   background-color: ${(props) => props.backgrounColor};
 `;
@@ -32,18 +33,18 @@ const ContainerIcon = styled.div`
 
 const Footer = () => {
   const itemsFooter = [
-    { icon: <VscRemote size={15} />, text: "", flex: 0, backgrounColor: "#2E8961" },
-    { icon: <BiGitBranch size={15} />, text: "main", flex: 0, backgrounColor: "" },
-    { icon: <VscError size={15} />, text: "0", flex: 0, backgrounColor: "" },
-    { icon: <VscWarning size={15} />, text: "0", flex: 1, backgrounColor: "" },
-    { icon: <VscCheck size={15} />, text: "", flex: 0, backgrounColor: "" },
-    { icon: <VscFeedback size={15} />, text: "", flex: 0, backgrounColor: "" },
-    { icon: <VscBell size={15} />, text: "", flex: 0, backgrounColor: "" },
+    { id: 0, icon: <VscRemote size={15} />, text: "", flex: 0, backgrounColor: "#2E8961" },
+    { id: 1, icon: <BiGitBranch size={15} />, text: "main", flex: 0, backgrounColor: "" },
+    { id: 2, icon: <VscError size={15} />, text: "0", flex: 0, backgrounColor: "" },
+    { id: 3, icon: <VscWarning size={15} />, text: "0", flex: 1, backgrounColor: "" },
+    { id: 4, icon: <VscCheck size={15} />, text: "", flex: 0, backgrounColor: "" },
+    { id: 5, icon: <VscFeedback size={15} />, text: "", flex: 0, backgrounColor: "" },
+    { id: 6, icon: <VscBell size={15} />, text: "", flex: 0, backgrounColor: "" },
   ];
 
   return <StyledFooter backgrounColor={"#3279CB"}>
     {itemsFooter.map((item) => (
-      <ContainerIcon flex={item.flex} backgrounColor={item.backgrounColor}>
+      <ContainerIcon key={item.id} flex={item.flex} backgrounColor={item.backgrounColor}>
         {item.icon}
         {item.text}
       </ContainerIcon>
